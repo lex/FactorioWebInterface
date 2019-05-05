7042f3292bf4f6fa7a15329ce1fbf61cbf9ebd41
James <jrdgillham@gmail.com>
2019-05-05 20:59:36 +0100

    updates

--- FactorioWebInterface/src/ts/mods.ts
+++ FactorioWebInterface/src/ts/mods.ts
@@ -22,9 +22,10 @@ import * as $ from "jquery";
     }
 
     const modPacksTable = document.getElementById('modPacksTable') as HTMLTableElement;
-
     const newModPackButton = document.getElementById('newModPackButton') as HTMLButtonElement;
+    const modPackFilesDiv = document.getElementById('modPackFilesDiv') as HTMLDivElement;
 
+    const fileTable = document.getElementById('fileTable') as HTMLTableElement;
 
     const newModPackModal = document.getElementById('newModPackModal') as HTMLDivElement;
     const newModPackModalBackground = document.getElementById('newModPackModalBackground') as HTMLDivElement;
@@ -40,6 +41,9 @@ import * as $ from "jquery";
 
     // XSRF/CSRF token, see https://docs.microsoft.com/en-us/aspnet/core/security/anti-request-forgery?view=aspnetcore-2.1
     let requestVerificationToken = (document.querySelector('input[name="__RequestVerificationToken"][type="hidden"]') as HTMLInputElement).value
+
+    let modPackTableBody = modPacksTable.tBodies[0];
+
 
     let renameOldName: string = null;
     let currentModPack: string = null;
@@ -70,12 +74,11 @@ import * as $ from "jquery";
     }
 
     function updateModPacks(modPacks: ModPackMetaData[]) {
-        let body = modPacksTable.tBodies[0];
-
-        body.innerHTML = "";
+        modPackTableBody.innerHTML = "";
 
         for (let modPack of modPacks) {
             let row = document.createElement('tr');
+            row.onclick = modPackRowClick;
 
             let cell1 = document.createElement('td');
             cell1.innerText = modPack.Name;
@@ -101,11 +104,13 @@ import * as $ from "jquery";
             cell4.appendChild(deleteButton);
             row.appendChild(cell4);
 
-            body.appendChild(row);
+            modPackTableBody.appendChild(row);
         }
     }
 
     function renameModPack(this: HTMLButtonElement, ev: MouseEvent) {
+        ev.stopPropagation();
+
         let row = this.parentElement.parentElement;
         let child = row.firstElementChild as HTMLElement;
         let name = child.innerText
@@ -118,6 +123,8 @@ import * as $ from "jquery";
     }
 
     async function deleteModPack(this: HTMLButtonElement, ev: MouseEvent) {
+        ev.stopPropagation();
+
         let row = this.parentElement.parentElement;
         let child = row.firstElementChild as HTMLElement;
         let name = child.innerText
@@ -129,15 +136,71 @@ import * as $ from "jquery";
         }
     }
 
+    async function modPackRowClick(this: HTMLTableRowElement, ev: MouseEvent) {
+        let child = this.firstElementChild as HTMLElement;
+        let modPack = child.innerText
+
+        currentModPack = modPack;
+
+        let files = await connection.invoke('GetModPackFiles', modPack) as ModPackFileMetaData[];
+
+        updateModPackFiles(modPack, files);
+    }
+
     function updateModPackFiles(modPack: string, files: ModPackFileMetaData[]) {
+        if (modPack === null) {
+            modPackFilesDiv.classList.add('is-invisible');
+            return;
+        }
+
+        modPackFilesDiv.classList.remove('is-invisible');
+
+        console.log(files);
+
+        let body = fileTable.tBodies[0];
+        body.innerHTML = "";
+
+        for (let file of files) {
+            let row = document.createElement('tr');
+
+            let cell1 = document.createElement('td');
+            let checkbox = document.createElement('input') as HTMLInputElement;
+            checkbox.type = 'checkbox';
+            checkbox.setAttribute('data-name', file.Name);
+            cell1.appendChild(checkbox);
+            row.appendChild(cell1);
+
+            let cell2 = document.createElement('td');
+            let link = document.createElement('a') as HTMLAnchorElement;
+            link.innerText = file.Name;
+            link.href = `/admin/mods?handler=file&modPack=${currentModPack}&fileName=${file.Name}`;
+            cell2.appendChild(link);
+            row.appendChild(cell2);
+
+            let cell3 = document.createElement('td');
+            cell3.innerText = formatDate(file.LastModifiedTime);
+            row.appendChild(cell3);
+
+            let cell4 = document.createElement('td');
+            cell4.innerText = bytesToSize(file.Size);
+            cell4.setAttribute('data-size', file.Size.toString());
+            row.appendChild(cell4);
+
+            body.appendChild(row);
+        }
+    }
+
+    function sendModPackFiles(modPack: string, files: ModPackFileMetaData[]) {
         if (modPack !== currentModPack) {
             return;
         }
+
+        updateModPackFiles(modPack, files);
     }
 
     connection.on("SendModPacks", updateModPacks)
 
-    connection.on("SendModPackFiles", updateModPackFiles)
+    connection.on("SendModPackFiles", sendModPackFiles)
 
     newModPackButton.onclick = () => {
         newModPackModal.classList.add('is-active');
@@ -210,6 +273,19 @@ import * as $ from "jquery";
         let min = pad(date.getUTCMinutes());
         let sec = pad(date.getUTCSeconds());
         return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
+    }
+
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    function bytesToSize(bytes: number) {
+        // https://gist.github.com/lanqy/5193417
+
+        if (bytes === 0)
+            return 'n/a';
+        const i = Math.floor(Math.log(bytes) / Math.log(1024));
+        if (i === 0)
+            return `${bytes} ${sizes[i]}`;
+        else
+            return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
     }
 
     start();
